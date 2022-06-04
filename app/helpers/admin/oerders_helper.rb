e8b9ff9f05712f1d330c37b234975c67b26cef58
omochi8311 <omochi.1126.tmc@gmail.com>
2022-06-04 13:36:13 +0000

    [Finish]Bootstrapインストール

---
+++ app/helpers/admin/oerders_helper.rb
@@ -0,0 +1,2 @@
+module Admin::OerdersHelper
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "83163e75bddc4d128340ada648e3a076.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
