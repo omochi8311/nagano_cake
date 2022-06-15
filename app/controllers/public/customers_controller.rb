d444c05710eabc43faab061aa40800012785e1f4
omochi8311 <omochi.1126.tmc@gmail.com>
2022-06-15 15:02:11 +0000

    [Update]マイページ編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,12 @@
 class Public::CustomersController < ApplicationController
+
+ def show
+    @customer = Customer.find(params[:id])
+ end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number)
+  end
 end
