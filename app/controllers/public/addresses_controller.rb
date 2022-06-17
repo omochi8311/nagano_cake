166cfdd9158d2e845940be9951ed91b59edb186d
omochi8311 <omochi.1126.tmc@gmail.com>
2022-06-17 07:14:20 +0000

    [Update]退会ページ編集

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,17 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def edit
+    @address =Address.find(params[:id])
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "public/homes#top"
-    # URL /customers/sign_in ...
+  get 'customers/my_page' => 'public/customers#show'
+  get '/customers/withdrawal' => 'public/customers#withdrawal'
+
+  # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -12,7 +15,6 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  get 'my_page' => 'public/customers#show'
   get 'about' => 'public/homes#about'
   get 'admin' => 'admin/homes#top'
 
