635bbf00a2fd113501a525225215e5beff1f197c
omochi8311 <omochi.1126.tmc@gmail.com>
2022-06-22 09:27:18 +0000

    [Start]カート内情報

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,6 +5,11 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total = 0
+  end
+  def destroy_all
+  CartItem.destroy_all
+  redirect_to cart_items_path
   end
 
   def create
